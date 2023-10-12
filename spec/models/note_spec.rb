f11d157b8f465c7b59ffe4acb390a2efa5f20164
yman0784 <tykesam@yahoo.co.jp>
2023-10-12 15:34:52 +0900

    Finish chapter8

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,32 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  # ファクトリで関連するデータを生成する
-  it "generates associated data from a factory" do
-    note = FactoryBot.create(:note)
-    puts "This note's project is #{note.project.inspect}"
-    puts "This note's user is #{note.user.inspect}"
-  end
-
-  before do
-    @user = User.create(
-      first_name: "Joe",
-      last_name:  "Tester",
-      email:      "joetester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
-    )
-
-    @project = @user.projects.create(
-      name: "Test Project",
-    )
-  end
+  let(:user) { FactoryBot.create(:user) }
+  let(:project) { FactoryBot.create(:project, owner: user) }
 
   # ユーザー、プロジェクト、メッセージがあれば有効な状態であること
   it "is valid with a user, project, and message" do
     note = Note.new(
       message: "This is a sample note.",
-      user: @user,
-      project: @project,
+      user: user,
+      project: project,
     )
     expect(note).to be_valid
   end
@@ -40,26 +23,35 @@ RSpec.describe Note, type: :model do
 
   # 文字列に一致するメッセージを検索する
   describe "search message for a term" do
-    before do
-      @note1 = @project.notes.create(
+    let!(:note1) {
+      FactoryBot.create(:note,
+        project: project,
+        user: user,
         message: "This is the first note.",
-        user: @user,
-      )
-      @note2 = @project.notes.create(
+        )
+    }
+
+    let!(:note2) {
+      FactoryBot.create(:note,
+        project: project,
+        user: user,
         message: "This is the second note.",
-        user: @user,
-      )
-      @note3 = @project.notes.create(
+        )
+    }
+
+    let!(:note3) {
+      FactoryBot.create(:note,
+        project: project,
+        user: user,
         message: "First, preheat the oven.",
-        user: @user,
-      )
-    end
+              )
+    }
 
     # 一致するデータが見つかるとき
     context "when a match is found" do
       # 検索文字列に一致するメモを返すこと
       it "returns notes that match the search term" do
-        expect(Note.search("first")).to include(@note1, @note3)
+        expect(Note.search("first")).to include(note1, note3)
       end
     end
 
@@ -68,6 +60,7 @@ RSpec.describe Note, type: :model do
       it "returns an empty collection" do
         # 空のコレクションを返すこと
         expect(Note.search("message")).to be_empty
+        expect(Note.count).to eq 3
       end
     end
   end
---
+++ spec/models/task_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+RSpec.describe Note, type: :model do
+  let(:user) { FactoryBot.create(:user) }
+  let(:project) { FactoryBot.create(:project, owner: user) }
+
+  # ユーザー、プロジェクト、メッセージがあれば有効な状態であること
+  it "is valid with a user, project, and message" do
+    note = Note.new(
+message: "This is a sample note.",
+      user: user,
+      project: project,
+          )
+    expect(note).to be_valid
+  end
+
+  # メッセージがなければ無効な状態であること
+  it "is invalid without a message" do
+    note = Note.new(message: nil)
+    note.valid?
+    expect(note.errors[:message]).to include("can't be blank")
+  end
+
+  # 文字列に一致するメッセージを検索する
+  describe "search message for a term" do
+    let!(:note1) {
+      FactoryBot.create(:note,
+        project: project,
+        user: user,
+        message: "This is the first note.",
+      )
+    }
+
+    let!(:note2) {
+      FactoryBot.create(:note,
+        project: project,
+        user: user,
+        message: "This is the second note.",
+      )
+    }
+
+    let!(:note3) {
+      FactoryBot.create(:note,
+        project: project,
+        user: user,
+        message: "First, preheat the oven.",
+      )
+    }
+
+    #一致するデータが見つかるとき
+    context "when a match is found" do
+      it "returns notes that match the search term" do
+        expect(Note.search("first")).to include(note1, note3)
+        expect(Note.search("first")).to_not include(note2)
+      end
+    end
+
+    # 一致するデータが1件も見つからないとき
+    context "when no match is found" do
+      # 空のコレクションを返すこと
+      it "returns an empty collection" do
+        expect(Note.search("message")).to be_empty
+        expect(Note.count).to eq 3
+      end
+    end
+  end
+end
