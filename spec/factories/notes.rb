40144ac2f18b8b031105fa6b54f44f967ef1e69a
yman0784 <tykesam@yahoo.co.jp>
2023-10-13 15:24:55 +0900

    rename .ignore

--- spec/factories/notes.rb
+++ spec/factories/notes.rb
@@ -3,5 +3,9 @@ FactoryBot.define do
     message { "My important note." }
     association :project
     user { project.owner }
+
+    trait :with_attachment do
+      attachment { Rack::Test::UploadFile.new( \"#{Rails.toot}/spec/filses/attachment.jpg", 'image/jpeg')}
+    end
   end
 end
